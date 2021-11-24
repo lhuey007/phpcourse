42096347963e4e46a59ac6f695d48a1412368ff2
Leigh Huey <lhuey.007@gmail.com>
2021-11-24 11:06:32 +0000

    Changed company name to LLC

--- variables-new.php
+++ variables-new.php
@@ -1,6 +1,6 @@
 <?php
 	
-	$company_name = "Guitar World, Inc.";
+	$company_name = "Guitar World, LLC.";
 	$tag_line = "Teaching Guitar since 2001";
 	$copy_right = "Copyright (c) " . $company_name . " " . date("Y") . " - All Rights Reserved";
 
